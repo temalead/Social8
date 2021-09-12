14af54f0b367d7a8bf194d8fddc390583b50c3c2
tema <itdetectiv@gmail.com>
2021-09-12 12:21:51 +0400

    npm

---
+++ src/main/java/social/net/controller/SimpleController.java
@@ -0,0 +1,12 @@
+package social.net.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @GetMapping("/message")
+    public String getMessageHello(){
+        return "hello";
+    }
+}
