2deea0abf6dbd4f2548054cd90baf2a369a705f8
Jet-Green <grishadzyin@gmail.com>
2021-09-15 20:27:45 +0400

    fix2

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -1,32 +1,13 @@
 <template>
-<<<<<<< HEAD
-    <div>
-        <hello-world></hello-world>
-    </div>
-=======
-  <v-app>
-    <router-view />
-  </v-app>
->>>>>>> 04ca1c4909ac0f23a3455fc2137105bade4522d6
+  <div>
+    <Main></Main>
+  </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-    import HelloWorld from "./views/HelloWorld";
-
-    export default {
-        name: "App",
-        components: {
-            HelloWorld
-        }
-    };
-=======
+import Main from "./views/Main.vue";
 export default {
   name: "App",
-
-  data: () => ({
-    //
-  }),
+  components: { Main },
 };
->>>>>>> 04ca1c4909ac0f23a3455fc2137105bade4522d6
 </script>
