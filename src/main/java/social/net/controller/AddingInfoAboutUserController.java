0f12cd85e28e260d497fc2cbf95a3fa78d8f5b68
tema <itdetectiv@gmail.com>
2021-09-22 22:21:36 +0400

    Added InfoUserController

---
+++ src/main/java/social/net/controller/AddingInfoAboutUserController.java
@@ -0,0 +1,28 @@
+package social.net.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import social.net.model.Pupil;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/profile")
+public class AddingInfoAboutUserController {
+    @GetMapping
+    public Pupil getProfileInfo(
+            @AuthenticationPrincipal Pupil pupil
+    ){
+        return pupil;
+    }
+    @PostMapping
+    public Pupil updateDataProfile(
+            @Valid Pupil pupil
+    ){
+        return null;
+    }
+}
