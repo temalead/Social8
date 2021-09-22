0b9e48c592b426e29054919571ff0f0031285b22
tema <itdetectiv@gmail.com>
2021-09-22 22:24:42 +0400

    Added InfoUserController

--- src/main/java/social/net/controller/AddingInfoAboutUserController.java
+++ src/main/java/social/net/controller/AddingInfoAboutUserController.java
@@ -1,6 +1,5 @@
 package social.net.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
