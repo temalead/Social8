b2cda850ec336d6817e6cba6026b38ac748f08ff
tema <itdetectiv@gmail.com>
2021-10-18 23:16:00 +0400

    Vk Api connecting to app

--- src/main/java/social/net/Main.java
+++ src/main/java/social/net/Main.java
@@ -1,10 +1,12 @@
 package social.net;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.vk.api.sdk.client.actors.UserActor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
+import social.net.service.vk.VkInitializer;
 
 @SpringBootApplication
 @EnableAsync
@@ -12,6 +14,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @PropertySource(name="EncryptedProperties", value = "classpath:encrypted.properties")
 public class Main {
     public static void main(String[] args) {
+        VkInitializer vkInitializer = new VkInitializer();
+        vkInitializer.getActor();
+
         SpringApplication.run(Main.class,args);
     }
 }
--- src/main/java/social/net/service/vk/VkInitializer.java
+++ src/main/java/social/net/service/vk/VkInitializer.java
@@ -2,12 +2,40 @@ package social.net.service.vk;
 
 import com.vk.api.sdk.client.TransportClient;
 import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.ServiceActor;
+import com.vk.api.sdk.client.actors.UserActor;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.streaming.responses.GetServerUrlResponse;
+import com.vk.api.sdk.queries.groups.GroupsGetQuery;
+import com.vk.api.sdk.queries.users.UsersGetQuery;
+import com.vk.api.sdk.streaming.clients.VkStreamingApiClient;
+import com.vk.api.sdk.streaming.clients.actors.StreamingActor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
 public class VkInitializer {
+    @Value("${access.token}")
+    private String accessToken;
+    TransportClient transportClient = new HttpTransportClient();
 
-    TransportClient transportClient=new HttpTransportClient();
-    VkApiClient vk=new VkApiClient(transportClient);
+    VkApiClient vk = new VkApiClient(transportClient);
+    VkStreamingApiClient streamingClient = new VkStreamingApiClient(transportClient);
+
+    //Create service actor
+    Integer appId = -175711177;
+    UserActor actor = new UserActor(appId, accessToken);
+
+    public void getActor() {
+        UsersGetQuery usersGetQuery = vk.users().get(actor);
+        UsersGetQuery fields = usersGetQuery.fields();
+        System.out.println(fields);
+    }
+
 }
+/*access_token=44a9cbf4ad7f51d2473c44418669dce16e806d557b6de0827f0b093bcf3e47c94b5d3e4ada17da93db3a8&expires_in=0
+&user_id=254530506*/
+
+
+/*https://oauth.vk.com/blank.html#access_token=44a9cbf4ad7f51d2473c44418669dce16e806d557b6de0827f0b093bcf3e47c94b5d3e4ada17da93db3a8
+&expires_in=0
+&user_id=254530506*/
