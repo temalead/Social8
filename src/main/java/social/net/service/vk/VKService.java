4e1bf5fe496c7600d416d844accf8e6559b01e8d
tema <itdetectiv@gmail.com>
2021-10-18 23:16:00 +0400

    Vk Api connecting to app

--- src/main/java/social/net/service/vk/VKService.java
+++ src/main/java/social/net/service/vk/VKService.java
@@ -2,11 +2,6 @@ package social.net.service.vk;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public class VKService {
-    private VkInitializer vk;
 
-    public VKService(VkInitializer vk) {
-        this.vk = vk;
-    }
 }
