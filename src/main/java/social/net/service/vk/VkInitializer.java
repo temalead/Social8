3f4ab1cd28c6f2949dc9104cd44c661fadb79c54
tema <itdetectiv@gmail.com>
2021-10-20 23:34:48 +0400

    Vk API 2. Formalize json

--- src/main/java/social/net/service/vk/VkInitializer.java
+++ src/main/java/social/net/service/vk/VkInitializer.java
@@ -1,16 +1,25 @@
 package social.net.service.vk;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.vk.api.sdk.client.TransportClient;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.users.UserFull;
 import com.vk.api.sdk.objects.users.responses.SearchResponse;
 import com.vk.api.sdk.streaming.clients.VkStreamingApiClient;
 import org.springframework.beans.factory.annotation.Value;
+import social.net.model.Pupil;
+import social.net.repo.PupilRepo;
+
+import java.util.List;
 
 public class VkInitializer {
+    private PupilRepo pupilRepo;
+
     @Value("${access.token}")
     private String token;
     TransportClient transportClient = new HttpTransportClient();
@@ -22,9 +31,26 @@ public class VkInitializer {
     Integer appId = 7978813;
     UserActor actor = new UserActor(appId, token);
 
+    public VkInitializer(PupilRepo pupilRepo) {
+        this.pupilRepo = pupilRepo;
+    }
+
     public SearchResponse getActor() throws ClientException, ApiException {
         SearchResponse execute = vk.users().search(actor).school(226069).count(1000).execute();
         return execute;
+    }
+    public boolean getDataOfUser(SearchResponse execute){
+        List<UserFull> items = execute.getItems();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+        if (!pupilRepo.findById((long) 123).isPresent()) {
+            for (UserFull item : items) {
+                Pupil pupil = gson.fromJson(item.toPrettyString(), Pupil.class);
+                pupilRepo.save(pupil);
+            }
+            return true;
+        }
+        return false;
     }
 
 }
